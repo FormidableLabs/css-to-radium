98eff9b4d86c7ea312e43091051f35ee8f3aa4a9
Ryan Roemer <ryan@loose-bits.com>
2015-03-09 10:40:19 -0700

    Add travis. Add in streams and handle stdin/stdout.

--- bin/cli.js
+++ bin/cli.js
@@ -1,17 +1,35 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
+var through2 = require('through2');
 
 var minimist = require('minimist');
 var stringifyObject = require('stringify-object');
 
 var convertCss = require('../index.js');
 
-var sourceCss = process.argv[2];
-var destJs = process.argv[3];
-
+// ----------------------------------------------------------------------------
+// Arguments
+// ----------------------------------------------------------------------------
 var cliArgs = minimist(process.argv);
 
+// TODO: DOCUMENT THIS.
+// TODO: Consider multiple flags instead for multiple inputs?
+var inputs = (cliArgs.input || '').split(',');
+
+// TODO: Handle 2+ inputs.
+var inputs = inputs[0] || null;
+
+// First two args are 'node' and script path.
+// Index 3 (file path) else stdin output arguments in remainder.
+if (cliArgs._.length > 3) {
+  throw new Error('Must have 0 or 1 output paths specified');
+}
+var output = cliArgs._[2] || null; // Remaining argument.
+
+// ----------------------------------------------------------------------------
+// Helpers
+// ----------------------------------------------------------------------------
 var getTabString = function (tabWidth) {
   var result = '';
 
@@ -29,23 +47,36 @@ var stringifyResult = function (data) {
   });
 };
 
-var writeJS = function (convertedData) {
-  var stringData = "module.exports = " + stringifyResult(convertedData) + ";";
-
-  if (destJs) {
-    fs.writeFile(destJs, stringData, function (err) {
-      if (err) throw err;
-    });
-
-    return;
-  }
-
-  console.log(stringData);
-};
-
-fs.readFile(sourceCss, 'utf-8', function (err, data) {
-  if (err) throw err;
-
-  var converted = convertCss(data);
-  writeJS(converted);
+// ----------------------------------------------------------------------------
+// Streams
+// ----------------------------------------------------------------------------
+// Converter stream.
+var _buffer = [];
+var convertStream = through2(function (chunk, enc, callback) {
+  _buffer.push(chunk.toString('utf8'));
+  callback();
+}, function (callback) {
+  var data = _buffer.join('');
+  var cssObj = convertCss(data);
+  var converted = new Buffer(JSON.stringify(cssObj));
+  this.push(converted);
+  callback();
 });
+
+// Encoding.
+process.stdin.setEncoding('utf8');
+
+// Input: File or stdin.
+var inputStream = inputs ?
+  fs.createReadStream(inputs, { encoding: 'utf8' }) :
+  process.stdin;
+
+// Output: File or stdout.
+var outputStream = output ?
+  fs.createWriteStream(output) :
+  process.stdout;
+
+// Pipe it!
+inputStream
+  .pipe(convertStream)
+  .pipe(outputStream);

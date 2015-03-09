9a8f976ca59fbbb64ca2a88286aa2f8ec77aaf5f
Alex Lande <alexlande@gmail.com>
2015-03-09 13:04:04 -0700

    Accept multiple inputs, update docs

--- bin/cli.js
+++ bin/cli.js
@@ -4,28 +4,23 @@ var fs = require('fs');
 var through2 = require('through2');
 var minimist = require('minimist');
 var stringifyObject = require('stringify-object');
+var Kat = require('kat');
 
 var convertCss = require('../index.js');
 
 // ----------------------------------------------------------------------------
 // Arguments
 // ----------------------------------------------------------------------------
-var cliArgs = minimist(process.argv);
+var cliArgs = minimist(process.argv.slice(2));
 
-// TODO: DOCUMENT THIS.
-// TODO: Consider multiple flags instead for multiple inputs?
-var inputs = (cliArgs.input || '').split(',');
+var inputs = cliArgs.input;
 
-// TODO: Handle 2+ inputs.
-var inputs = inputs[0] || null;
-
-// First two args are 'node' and script path.
-// Index 3 (file path) else stdin output arguments in remainder.
-if (cliArgs._.length > 3) {
+// Index 0 (file path) else stdin output arguments in remainder.
+if (cliArgs._.length > 1) {
   throw new Error('Must have 0 or 1 output paths specified');
 }
 
-var output = cliArgs._[2] || null; // Remaining argument.
+var output = cliArgs._[0] || null; // Remaining argument.
 
 // ----------------------------------------------------------------------------
 // Helpers
@@ -66,10 +61,16 @@ var convertStream = through2(function (chunk, enc, callback) {
 // Encoding.
 process.stdin.setEncoding('utf8');
 
-// Input: File or stdin.
+// Input: Files or stdin.
 var inputStream = inputs ?
-  fs.createReadStream(inputs, { encoding: 'utf8' }) :
+  new Kat() :
   process.stdin;
+
+if (inputs) {
+  inputs.forEach(function (input) {
+    inputStream.add(input);
+  });
+}
 
 // Output: File or stdout.
 var outputStream = output ?
